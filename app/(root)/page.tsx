a29e1e2fa44a7ba78ac86e28a6dd8f42c27abd5a
abhisek1221 <cherrymx317@gmail.com>
2024-01-11 19:55:15 +0530

    user authentication and base UI setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,9 +3,9 @@ import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black">
       <h1 className='text-4xl text-purple-600'>UrbaneX</h1>
-      <Button>Click me</Button>
+      <Button>Test Button</Button>
     </main>
   )
 }
