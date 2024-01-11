9a68b72a1ac01540388e38a12ca5823f7fd3c5f3
abhisek1221 <cherrymx317@gmail.com>
2024-01-11 20:32:09 +0530

    navbar & mobile responsive

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,7 +4,7 @@ import { Button } from '@/components/ui/button'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black">
-      <h1 className='text-4xl text-purple-600'>UrbaneX</h1>
+      <h1 className='text-4xl text-purple-600 font-extrabold'>UrbaneX</h1>
       <Button>Test Button</Button>
     </main>
   )
--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -7,8 +7,9 @@ import {
     SheetTitle,
     SheetTrigger,
   } from "@/components/ui/sheet"
-  import Image from 'next/image'
-  
+import Image from 'next/image'
+import { Separator } from "@/components/ui/separator"  
+import NavItems from './NavItems'
 
 const MobileNav = () => {
   return (
@@ -23,14 +24,16 @@ const MobileNav = () => {
             className="cursor-pointer"
             />
         </SheetTrigger>
-        <SheetContent className='flex flex-col '>
-            <SheetHeader>
-            <SheetTitle>Are you absolutely sure?</SheetTitle>
-            <SheetDescription>
-                This action cannot be undone. This will permanently delete your account
-                and remove your data from our servers.
-            </SheetDescription>
-            </SheetHeader>
+        <SheetContent className='flex flex-col gap-6 bg-white md:hidden'>
+        <Image
+            src="/assets/images/logo.jpeg"
+            alt='menu'
+            width={58}
+            height={38}
+            className="cursor-pointer"
+            />
+            <Separator className='border border-gray-200'/>
+            <NavItems />
         </SheetContent>
         </Sheet>
 
