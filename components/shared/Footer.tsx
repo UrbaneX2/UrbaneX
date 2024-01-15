dcb86041cfbe92ee8e15fab9463f929ab30d2934
abhisek1221 <cherrymx317@gmail.com>
2024-01-15 13:34:22 +0530

    minor bug fix in prod

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import Image from "next/image"
+import Link from "next/link"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="bg-black border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href='/'>
+          <Image 
+            src="/assets/images/logo.jpeg"
+            alt="logo"
+            width={58}
+            height={38}
+          />
+        </Link>
+
+        <p className="text-white">2023 UrbaneX. All Rights reserved.</p>
+      </div>
+    </footer>
   )
 }
 
