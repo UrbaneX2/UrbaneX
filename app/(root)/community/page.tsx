9b4884a317ee3c4612d2f89fed615e6fd0870f02
inodinwetrust10 <adi4gbsingh@gmail.com>
2024-01-26 19:33:06 +0530

    Community page

--- app/(root)/community/page.tsx
+++ app/(root)/community/page.tsx
@@ -1,26 +1,44 @@
+"use client";
+import { useEffect, useState } from "react";
 import Cards from "@/components/ui/cards";
-import Navbars from "@/components/ui/navbar";
+import { fetchAllIssues } from "@/lib/actions/issue.actions";
 import { Button } from "@nextui-org/react";
+
 export default function Home() {
+  const [allIssue, setAllIssue] = useState<any>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetchAllIssues();
+        setAllIssue(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-<>
-  <div className="main-container flex-col">
-    <section className="flex flex-row bg-gray-800 rounded-md lg:mt-0 fixed z-10">
-      <Button className="bg-green-500 px-4 lg:px-5 mb-2 hover:bg-green-600">
-        Open issues
-      </Button>
-      <Button className="bg-yellow-500 px-4 lg:px-5 mb-2 hover:bg-yellow-600">
-        InProgress issues
-      </Button>
-      <Button className="bg-red-500 px-4 lg:px-5 mb-2 hover:bg-red-600">
-        Closed issues
-      </Button>
-    </section>
-    <section className="flex flex-col bg-blue-500 gap-4 p-10 grow">
-      <Cards />
-      <Cards />
-    </section>
-  </div>
-</>
+    <div className="main-container flex flex-col ">
+      <section className="flex flex-wrap gap-4 items-center">
+        <Button color="success">Open Issues</Button>
+        <Button color="warning">Inprogress Issues</Button>
+        <Button color="danger">Closed Issues</Button>
+      </section>
+
+      <section className="flex flex-col lg:flex-grow bg-transparent p-4 lg:p-10 gap-4 rounded-md md:p-10">
+        {allIssue.map((issue) => (
+          <Cards
+            key={issue._id}
+            createdAt={issue.createdAt.toString()}
+            author={issue.author}
+            _id={issue._id}
+            text={issue.text}
+          />
+        ))}
+      </section>
+    </div>
   );
 }
--- lib/actions/issue.actions.ts
+++ lib/actions/issue.actions.ts
@@ -1,41 +1,36 @@
 "use server";
 
-import { connectToDB } from '../mongoose';
-import Issue from '../models/issue.model';
-import User from '../models/user.model';
-import { revalidatePath } from 'next/cache';
-import Community from '../models/community.model';
+import { connectToDB } from "../mongoose";
+import Issue from "../models/issue.model";
+import User from "../models/user.model";
+import { revalidatePath } from "next/cache";
+import Community from "../models/community.model";
 
 interface Params {
-    text: string,
-    author: string,
-    communityId: string | null,
-    path: string,
+  text: string;
+  author: string;
+  communityId: string | null;
+  path: string;
 }
 
-export async function createIssue({
-    text,
-    author,
-    communityId,
-    path
-}: Params) {
-    try {
-        connectToDB();
+export async function createIssue({ text, author, communityId, path }: Params) {
+  try {
+    connectToDB();
 
-        const communityIdObject = await Community.findOne(
-          { id: communityId },
-          { _id: 1 }
-        );
+    const communityIdObject = await Community.findOne(
+      { id: communityId },
+      { _id: 1 },
+    );
 
     const createdIssue = await Issue.create({
-        text,
-        author,
-        community: communityIdObject,
+      text,
+      author,
+      community: communityIdObject,
     });
 
     // Update user model
-    await User.findByIdAndUpdate(author,{
-        $push: {issues: createdIssue._id}
+    await User.findByIdAndUpdate(author, {
+      $push: { issues: createdIssue._id },
     });
 
     if (communityIdObject) {
@@ -45,79 +40,79 @@ export async function createIssue({
       });
     }
 
-    revalidatePath(path)
-    } catch (error: any) {
-        throw new Error(`Error creating issue: ${error.message}`);
-    }
-     
-    }
+    revalidatePath(path);
+  } catch (error: any) {
+    throw new Error(`Error creating issue: ${error.message}`);
+  }
+}
 
 export async function fetchPosts(pageNumber = 1, pageSize = 15) {
+  connectToDB();
 
-    connectToDB();
+  const skipAmount = (pageNumber - 1) * pageSize;
 
-    const skipAmount = (pageNumber - 1) * pageSize;
+  // fetch the posts that have no parents (top level issues)
 
-    // fetch the posts that have no parents (top level issues)
-
-    const postsQuery = Issue.find({ parentId: { $in: [null, undefined] } })
+  const postsQuery = Issue.find({ parentId: { $in: [null, undefined] } })
     .sort({ createdAt: "desc" })
     .skip(skipAmount)
     .limit(pageSize)
     .populate({
       path: "author",
       model: User,
-    }).populate({
+    })
+    .populate({
       path: "community",
       model: Community,
     })
     .populate({
-      path: "children", 
+      path: "children",
       populate: {
-        path: "author", 
+        path: "author",
         model: User,
         select: "_id name parentId image", // Select only _id and username fields of the author
       },
     });
 
-    const totalPostsCount = await Issue.countDocuments({
-        parentId: { $in: [null, undefined] },
-      });
+  const totalPostsCount = await Issue.countDocuments({
+    parentId: { $in: [null, undefined] },
+  });
 
-      const posts = await postsQuery.exec();
+  const posts = await postsQuery.exec();
 
-      const isNext = totalPostsCount > skipAmount + posts.length;
+  const isNext = totalPostsCount > skipAmount + posts.length;
 
-      return { posts, isNext };
-    }
-
+  return { posts, isNext };
+}
 
 export async function fetchIssueById(id: string) {
-    connectToDB();
+  connectToDB();
 
-    try {
-        const issue = await Issue.findById(id).
-        populate({
-            path: "author",
-            model: User,
-            select: "_id id name image",
-    }).populate({
-      path: "community",
-      model: Community,
-      select: "_id id name image",
-    }).populate({
-        path: "children", 
+  try {
+    const issue = await Issue.findById(id)
+      .populate({
+        path: "author",
+        model: User,
+        select: "_id id name image",
+      })
+      .populate({
+        path: "community",
+        model: Community,
+        select: "_id id name image",
+      })
+      .populate({
+        path: "children",
         populate: [
           {
-            path: "author", 
+            path: "author",
             model: User,
-            select: "_id id name parentId image", 
+            select: "_id id name parentId image",
           },
           {
-            path: "children", 
-            model: Issue, 
+            path: "children",
+            model: Issue,
             populate: {
-              path: "author", 
+              path: "author",
               model: User,
               select: "_id id name parentId image",
             },
@@ -133,35 +128,33 @@ export async function fetchIssueById(id: string) {
 }
 
 export async function addCommentToIssue(
-    issueId: string,
-    commentText: string,
-    userId: string,
-    path: string,
+  issueId: string,
+  commentText: string,
+  userId: string,
+  path: string,
 ) {
-    connectToDB();
+  connectToDB();
 
-    try {
+  try {
+    const originalIssue = await Issue.findById(issueId);
+    if (!originalIssue) throw new Error("Issue not found");
 
-        const originalIssue = await Issue.findById(issueId);
-        if(!originalIssue) throw new Error("Issue not found");
+    const commentIssue = new Issue({
+      text: commentText,
+      author: userId,
+      parentId: issueId,
+    });
 
-        const commentIssue = new Issue({
-            text: commentText,
-            author: userId,
-            parentId: issueId,
-        });
+    const savedCommentIssue = await commentIssue.save();
 
-        const savedCommentIssue = await commentIssue.save();
+    originalIssue.children.push(savedCommentIssue._id);
 
-        originalIssue.children.push(savedCommentIssue._id);
+    await originalIssue.save();
 
-        await originalIssue.save();
-
-        revalidatePath(path);
-
-    } catch (error: any) {
-        throw new Error(`Error adding comment to issue: ${error.message}`);
-    }
+    revalidatePath(path);
+  } catch (error: any) {
+    throw new Error(`Error adding comment to issue: ${error.message}`);
+  }
 }
 
 async function fetchAllChildIssues(issueId: string): Promise<any[]> {
@@ -187,41 +180,49 @@ export async function deleteIssue(id: string, path: string): Promise<void> {
 
     const descendantIssues = await fetchAllChildIssues(id);
 
- 
     const descendantIssueIds = [
       id,
       ...descendantIssues.map((issue) => issue._id),
     ];
 
-
     const uniqueAuthorIds = new Set(
       [
         ...descendantIssues.map((issue) => issue.author?._id?.toString()),
         mainIssue.author?._id?.toString(),
-      ].filter((id) => id !== undefined)
+      ].filter((id) => id !== undefined),
     );
 
     const uniqueCommunityIds = new Set(
       [
-        ...descendantIssues.map((issue) => issue.community?._id?.toString()), 
+        ...descendantIssues.map((issue) => issue.community?._id?.toString()),
         mainIssue.community?._id?.toString(),
-      ].filter((id) => id !== undefined)
+      ].filter((id) => id !== undefined),
     );
 
     await Issue.deleteMany({ _id: { $in: descendantIssueIds } });
 
     await User.updateMany(
       { _id: { $in: Array.from(uniqueAuthorIds) } },
-      { $pull: { issues: { $in: descendantIssueIds } } }
+      { $pull: { issues: { $in: descendantIssueIds } } },
     );
 
     await Community.updateMany(
       { _id: { $in: Array.from(uniqueCommunityIds) } },
-      { $pull: { issues: { $in: descendantIssueIds } } }
+      { $pull: { issues: { $in: descendantIssueIds } } },
     );
 
     revalidatePath(path);
   } catch (error: any) {
     throw new Error(`Failed to delete issue: ${error.message}`);
+  }
+}
+export async function fetchAllIssues() {
+  await connectToDB();
+
+  try {
+    const allIssues = await Issue.find({ parentId: { $exists: false } });
+    return allIssues;
+  } catch (err) {
+    throw new Error("Can't fetch all issues");
   }
 }
